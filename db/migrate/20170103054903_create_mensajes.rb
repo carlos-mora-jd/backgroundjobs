00d27b39f4de64fe2313a986a0781ce7f5b1245b
Carlos Mora <carlos.mora@janeirodigital.com>
2017-01-03 11:26:39 -0600

    Removing files

--- db/migrate/20170103054903_create_mensajes.rb
+++
@@ -1,9 +0,0 @@
-class CreateMensajes < ActiveRecord::Migration[5.0]
-  def change
-    create_table :mensajes do |t|
-      t.string :mensaje
-
-      t.timestamps
-    end
-  end
-end
--- test/controllers/mensajes_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MensajesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get mensajes_new_url
-    assert_response :success
-  end
-
-end
